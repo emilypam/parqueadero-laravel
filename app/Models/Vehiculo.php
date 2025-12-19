f97bf3306054b27f99d8db1d9d4e70fe90e6b4f3
emilypam <emilyp131204@gmail.com>
2025-12-18 19:26:16 -0500

    CRUD Parqueadero completo

---
+++ app/Models/Vehiculo.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Vehiculo extends Model
+{
+    protected $table = 'vehiculos';
+
+    protected $fillable = [
+        'placa',
+        'tipo',
+        'propietario',
+        'observaciones'
+    ];
+
+    static public function getVehiculos()
+    {
+        return self::all();
+    }
+
+    static public function getVehiculosById($id)
+    {
+        return self::find($id);
+    }
+
+    static public function createVehiculo(array $data)
+    {
+        return self::create($data);
+    }
+
+    public function updateVehiculo(array $data)
+    {
+        return $this->update($data);
+    }
+
+    static public function deleteVehiculo(Vehiculo $vehiculo)
+    {
+        $vehiculo->delete();
+    }
+}
